d90df055906c14be1ffef9c736ef320e63b496be
Cuong Nguyen <cuong.shodan@gmail.com>
2024-07-27 00:41:15 +0200

    add functionality for slides done!

--- assets/script.js
+++ assets/script.js
@@ -18,19 +18,12 @@ const slides = [
   },
 ];
 
-
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const dotsContainer = document.querySelector(".dots");
-
-arrowLeft.addEventListener("click", () => {
-  console.log("Left arrow clicked");
-});
-
-arrowRight.addEventListener("click", () => {
-  console.log("Right arrow clicked");
-});
-
+const bannerImg = document.querySelector('.banner-img');
+const bannerText = document.querySelector('#banner p');
+let currentSlideIndex = 0;
 
 // Create dots based on the number of slides
 slides.forEach((slide, index) => {
@@ -40,3 +33,38 @@ slides.forEach((slide, index) => {
   dot.setAttribute("data-index", index);
   dotsContainer.appendChild(dot);
 });
+
+const dots = document.querySelectorAll(".dot");
+
+
+
+function showSlide(index) {
+  bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+  bannerText.innerHTML = slides[index].tagLine;
+
+  dots.forEach((dot, dotIndex) => {
+    dot.classList.toggle("dot_selected", dotIndex === index);
+  });
+}
+
+arrowLeft.addEventListener("click", () => {
+  currentSlideIndex =
+    currentSlideIndex > 0 ? currentSlideIndex - 1 : slides.length - 1;
+  showSlide(currentSlideIndex);
+});
+
+arrowRight.addEventListener("click", () => {
+  currentSlideIndex =
+    currentSlideIndex < slides.length - 1 ? currentSlideIndex + 1 : 0;
+  showSlide(currentSlideIndex);
+});
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    currentSlideIndex = index;
+    showSlide(currentSlideIndex);
+  });
+});
+
+// Initialize the first slide
+showSlide(currentSlideIndex);
